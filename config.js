7a5d20d8c57104a3558da90c183ec9ebf8506aad
kyle12357 <kyle68699@gmail.com>
2024-11-09 15:42:51 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-  myUserConfig:{
-    username: 'kyle123',
-    password: '6db9e65964ed6e885267a7a9a625bf4ed39b099d2c2b7c1c5d162fb1482a0ac97e1408311d6b978c312f58a5c9d0ff001167863b5d5128abfdb2cddc9dd144ce',
-    logVerbosity: 3
-  },
-}
-
-
